72092088bc9bd7aa3f07007e43913b48c0f845cd
jdflyer <jdflyer10@gmail.com>
2021-12-26 19:42:15 -0700

    tab and uppercase support

--- src/kmain.c
+++ src/kmain.c
@@ -100,6 +100,9 @@ void tputchar(terminal_t* terminal, char c) {
             tSetCursorPos(terminal,terminal->cursorX-1,terminal->cursorY);
         }
         break;
+    case '\t':
+        tputs(terminal,"     ");
+        break;
     default:
         terminal->buffer[TBUFFERLOC(terminal,terminal->cursorX,terminal->cursorY)] = TTEXT(c,TCOLOR(terminal->textColor,terminal->bgColor));
         tSetCursorPos(terminal,terminal->cursorX+1,terminal->cursorY);
@@ -282,6 +285,9 @@ typedef struct{
     uint32_t ss;
 } interrupt_frame_t;
 
+bool shiftPressed = false;
+bool capsLock = false;
+
 uint8_t keyboardScancodesTable[128] =
 {
     0,  27, '1', '2', '3', '4', '5', '6', '7', '8',	/* 9 */
@@ -322,10 +328,67 @@ uint8_t keyboardScancodesTable[128] =
     0,	/* All other keys are undefined */
 };		
 
+uint8_t keyboardUpperScancodesTable[128] =
+{
+    0,  27, '!', '@', '#', '$', '%', '^', '&', '*',	/* 9 */
+  '(', ')', '_', '+', '\b',	/* Backspace */
+  '\t',			/* Tab */
+  'Q', 'W', 'E', 'R',	/* 19 */
+  'T', 'Y', 'U', 'I', 'O', 'P', '{', '}', '\n',	/* Enter key */
+    0,			/* 29   - Control */
+  'A', 'S', 'D', 'F', 'G', 'H', 'J', 'K', 'L', ':',	/* 39 */
+ '\"', '~',   0,		/* Left shift */
+ '|', 'Z', 'X', 'C', 'V', 'B', 'N',			/* 49 */
+  'M', '<', '>', '?',   0,				/* Right shift */
+  '*',
+    0,	/* Alt */
+  ' ',	/* Space bar */
+    0,	/* Caps lock */
+    0,	/* 59 - F1 key ... > */
+    0,   0,   0,   0,   0,   0,   0,   0,
+    0,	/* < ... F10 */
+    0,	/* 69 - Num lock*/
+    0,	/* Scroll Lock */
+    0,	/* Home key */
+    0,	/* Up Arrow */
+    0,	/* Page Up */
+  '-',
+    0,	/* Left Arrow */
+    0,
+    0,	/* Right Arrow */
+  '+',
+    0,	/* 79 - End key*/
+    0,	/* Down Arrow */
+    0,	/* Page Down */
+    0,	/* Insert Key */
+    0,	/* Delete Key */
+    0,   0,   0,
+    0,	/* F11 Key */
+    0,	/* F12 Key */
+    0,	/* All other keys are undefined */
+};		
+
 __attribute__((interrupt)) void keyboardInterruptHandler(interrupt_frame_t* frame) {
     uint8_t scancode = inb(0x60);
+    if(scancode==0x2a) {
+        shiftPressed = true;
+    }else if(scancode==0xaa) {
+        shiftPressed = false;
+    }
+    if(scancode==0x3a) {
+        if(capsLock==false) {
+            capsLock = true;
+        }else{
+            capsLock = false;
+        }
+    }
+
     if (keyboardScancodesTable[scancode]!=0) {
-        tputchar(&mainterm,keyboardScancodesTable[scancode]);
+        if(shiftPressed==false&&capsLock==false||shiftPressed==true&&capsLock==true) {
+            tputchar(&mainterm,keyboardScancodesTable[scancode]);
+        }else{
+            tputchar(&mainterm,keyboardUpperScancodesTable[scancode]);
+        }
     }
     PICSendEOI(1);
 }
